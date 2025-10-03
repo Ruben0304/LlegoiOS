be0f06662af02ca1e83160cfafadc2727969cd16
Ruben0304 <hernandzruben9@gmail.com>
2025-10-02 21:45:20 -0400

    Initial Commit

---
+++ LlegoiOS/LlegoiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  LlegoiOSApp.swift
+//  LlegoiOS
+//
+//  Created by Ruben on 2/10/25.
+//
+
+import SwiftUI
+
+@main
+struct LlegoiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
