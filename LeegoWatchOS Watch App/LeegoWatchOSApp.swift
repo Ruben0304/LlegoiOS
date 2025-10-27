7370792565500a9b85257ccad6678f28ec2c725b
Ruben0304 <hernandzruben9@gmail.com>
2025-10-27 12:31:30 -0400

    s

---
+++ LeegoWatchOS Watch App/LeegoWatchOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  LeegoWatchOSApp.swift
+//  LeegoWatchOS Watch App
+//
+//  Created by Ruben on 10/27/25.
+//
+
+import SwiftUI
+
+@main
+struct LeegoWatchOS_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
